b1804f6cecb69e630ec0bc89e43b59d5e1719afa
ikReza <ireza.kaiser00@gmail.com>
2020-08-25 00:38:18 +0600

    node-sass added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Homepage from "./pages/homepage/homepage.component";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 };
 
 export default App;
